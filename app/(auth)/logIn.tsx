0eb5008c4f44f8f522bcd2984bcf543a51ec9095
whitedeath0804 <bojidar.haanen.hacker.neo@gmail.com>
2025-08-24 13:53:12 +0300

    first commit

---
+++ app/(auth)/logIn.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const logIn = () => {
+  return (
+    <View>
+      <Text>logIn</Text>
+    </View>
+  )
+}
+
+export default logIn
+
+const styles = StyleSheet.create({})
---
+++ app/(auth)/signIN.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const signIN = () => {
+  return (
+    <View>
+      <Text>signIN</Text>
+    </View>
+  )
+}
+
+export default signIN
+
+const styles = StyleSheet.create({})
